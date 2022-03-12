d68bf7b122aec17a7e31c0efb2a36618860ebe28
thissideaditya <adityac486@gmail.com>
2022-03-12 21:49:09 +0530

    project started

---
+++ main.js
@@ -0,0 +1,15 @@
+// entry point of command line
+
+let inputArr = process.argv.slice(2)
+let command = inputArr[0]
+switch(command){
+    case "tree":
+        //call tree function
+        break
+    case "organise":
+    //call organise function
+        break
+    case "help":
+        //call help function
+        break
+}
