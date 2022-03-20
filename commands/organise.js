057b55789f0a017214a0964a1a6aad80a6dac3ed
thissideaditya <adityac486@gmail.com>
2022-03-20 20:02:09 +0530

    added some extensions and project is completed

--- commands/organise.js
+++ commands/organise.js
@@ -1,10 +1,10 @@
 const fs = require("fs"); //fs module
 const path = require("path"); //path module
 let types = {
-    media: ["mp4", "mkv", "mp3"],
+    media: ["mp4", "mkv", "mp3", "mov"],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
     documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
-    app: ['exe', 'dmg', 'pkg', "deb"],
+    app: ['exe', 'dmg', 'pkg', "deb", "apk"],
     images: ['png','jpg','jpeg']
 }
 
